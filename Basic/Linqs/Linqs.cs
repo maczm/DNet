1c236b421d3153bf2a1b84548b99ee967defbf7f
wangzm <1466418631@qq.com>
2025-10-13 17:23:59 +0800

    日常提交

--- Basic/Linqs/Linqs.cs
+++ Basic/Linqs/Linqs.cs
@@ -30,29 +30,25 @@ internal class Linqs
         Console.WriteLine("Linq Select ================================================================================= END");
         var names = objs.Select(x => x.Name).ToList();
         Console.WriteLine("[" + string.Join(", ", names) + "]");
-        List<string> phrases = ["an apple a day", "the quick brown fox"];
         Console.WriteLine("\n");
 
         Console.WriteLine("Linq SelectMany ============================================================================= END");
+        List<string> phrases = ["an apple a day", "the quick brown fox"];
         var query = phrases.SelectMany(phrase => phrase.Split(' '));
         Console.WriteLine("[" + string.Join(", ", query) + "]");
         Console.WriteLine("\n");
 
         Console.WriteLine("Linq SelectMany ============================================================================= END");
-        List<int> numbers = [1, 2, 3];
-        List<char> letters = ['a', 'b', 'c', 'd'];
-        var crossJoin = numbers
-            .SelectMany(number => letters,
-            (number, letter) => (number, letter));
+        List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        List<string> letters = ["1a", "2b", "3c", "4d", "5e", "f", "g", "h"];
+        var crossJoin = numbers.SelectMany(number => letters, (number, letter) => (number, letter));
         Console.WriteLine("[" + string.Join(", ", crossJoin) + "]");
         Console.WriteLine("\n");
 
         Console.WriteLine("Linq Zip ==================================================================================== END");
-        var a = numbers.Zip(letters);
+        List<string> emoji = [ "🤓", "🔥", "🎉", "👀", "⭐", "💜", "✔", "💯"];
+        var a = numbers.Zip(letters, emoji);
         Console.WriteLine("[" + string.Join(", ", a) + "]");
         Console.WriteLine("\n");
-
-        // A string array with 8 elements.
-        IEnumerable<string> emoji = [ "🤓", "🔥", "🎉", "👀", "⭐", "💜", "✔", "💯"];
     }
 }
